2095b45d268134548d773878840d8c9705b3db40
Tim Dela Cruz <etdc0418@gmail.com>
2024-11-25 01:51:36 -0800

    Deck creation and shuffle Implemented!
    DISCORD SERVER GOES HARD

--- scripts/deck.gd
+++ scripts/deck.gd
@@ -17,11 +17,12 @@ func deck_creator()-> Array[CardSpec]:
 	# Go through each row in the file and create a CardSpec object from it
 	while !deck_file.eof_reached():
 		curr_specs = Array(deck_file.get_csv_line())
-		month = CardSpec.Month.keys()[curr_specs[0]]
-		type = CardSpec.Type.keys()[curr_specs[1]]
-		synergy = CardSpec.Synergy.keys()[curr_specs[2]]
-		texture = curr_specs[2]
-		var curr_card:CardSpec = CardSpec.new(month, type, synergy, texture)
+		month = int(curr_specs[0])
+		type = int(curr_specs[1])
+		synergy = int(curr_specs[2])
+		#texture = curr_specs[3]
+		# TODO: instead of preloading temp properly load in the sprite name
+		var curr_card:CardSpec = CardSpec.new(month, type, synergy, preload("res://assets/temp.png"))
 		deck.append(curr_card)
 	deck_file.close()
 		
