9168f450007f2068501bc0676c04903a8b9a7df9
Yujin Cho <yujincho2002@gmail.com>
2024-12-09 20:30:55 -0800

    basic sidebar, enemy sprites addedd

--- scripts/screens/title_screen.gd
+++ scripts/screens/title_screen.gd
@@ -11,8 +11,3 @@ func _process(_delta):
 	if Input.is_action_just_pressed("ui_select"):
 		print("Switching to battle screen...")
 		signals.switch_scene.emit("battle_screen")
-
-func _switch_to_battle(event):
-	if Input.is_action_just_pressed("ui_select"):
-		print("Switching to battle screen...")
-		signals.switch_scene.emit("battle_screen")
