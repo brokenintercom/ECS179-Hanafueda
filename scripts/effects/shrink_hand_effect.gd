434390ebb218f251a09d7e00f93cfdaa94ac2aac
chr-wang <104956901+chr-wang@users.noreply.github.com>
2024-12-12 00:21:22 -0800

    Fix effect bugs

--- scripts/effects/shrink_hand_effect.gd
+++ scripts/effects/shrink_hand_effect.gd
@@ -1,36 +1,20 @@
 class_name ShrinkHandEffect
 extends Effect
 
-
 var _num_cards:int
 
 
-func _init(turns:int, num_cards:int) -> void:
-	super(turns)
+func _init(parent:Character, turns:int, num_cards:int) -> void:
+	super(parent, turns)
 	_num_cards = num_cards
 
 
 func _on_switch_battle_phase() -> void:
 	super()
 	
-	var parent := get_parent() as Character
-	assert(parent is Player, "Error: Target is not Player")
-	var player := parent as Player
+	assert(_parent is Player, "Error: Target is not Player")
+	var player := _parent as Player
 	if not player:
 		return
 	
 	player.hand.max_hand_size = clampi(player.hand.max_hand_size, 0, player.hand.MAX_HAND)
-
-
-# Reduce player hand size by amount
-func apply(target:Character, amount:float) -> void:
-	
-	
-	var player := target as Player
-	
-	if not player:
-		return
-	
-	# TODO can use clampi
-	player.hand.max_hand_size -= max(floor(amount), 0)
-	player.hand.max_hand_size = max(player.hand.max_hand_size, 0)
