f0c0b5fad5092777b33104a9d7d6b6cf9b12aa00
Jamie Hsi <jhsi05001@gmail.com>
2024-12-12 12:01:40 -0800

    added credit button to results screen

--- scripts/screens/results_screen.gd
+++ scripts/screens/results_screen.gd
@@ -24,3 +24,6 @@ func _on_back_button_pressed() -> void:
 
 func _on_quit_button_pressed() -> void:
 	get_tree().quit()
+
+func _on_credits_button_pressed() -> void:
+	signals.switch_scene.emit("tcredits_screen")
