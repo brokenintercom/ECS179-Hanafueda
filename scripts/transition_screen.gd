b9de7f848c444762208d48cff41a4d16b68c72d5
Yujin Cho <yujincho2002@gmail.com>
2024-12-13 15:59:03 -0800

    enemy goes back to idle once attacking

--- scripts/transition_screen.gd
+++ scripts/transition_screen.gd
@@ -1,22 +1,22 @@
 extends CanvasLayer
 
-@onready var color_rect = $ColorRect
-@onready var animation_player = $AnimationPlayer
+@onready var _color_rect = $ColorRect
+@onready var _animation_player = $AnimationPlayer
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	color_rect.visible = false
-	animation_player.animation_finished.connect(_on_animation_finished)
+	_color_rect.visible = false
+	_animation_player.animation_finished.connect(_on_animation_finished)
 
 
 func transition():
-	color_rect.visible = true
-	animation_player.play("fade_to_black")
+	_color_rect.visible = true
+	_animation_player.play("fade_to_black")
 
 
 func _on_animation_finished(anim_name):
 	if anim_name == "fade_to_black":
 		signals.on_transition_finished.emit()
-		animation_player.play("fade_from_black")
+		_animation_player.play("fade_from_black")
 	elif anim_name == "fade_from_black":
-		color_rect.visible = false
+		_color_rect.visible = false
